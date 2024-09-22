77c08de4e1fef6a469ea74f0e0ca79eb4801ea35
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-09-22 22:41:24 +0700

    update server ip

--- src/mindustrytool/Config.java
+++ src/mindustrytool/Config.java
@@ -13,7 +13,7 @@ public class Config {
 
     public static final Executor BACKGROUND_TASK_EXECUTOR = Executors.newSingleThreadExecutor();
 
-    public static final String SERVER_IP = "15.235.147.219";
+    public static final String SERVER_IP = "103.20.96.24";
     public static final String DISCORD_INVITE_URL = "https://discord.com/invite/DCX5yrRUyp";
     public static final String MINDUSTRY_TOOL_URL = "https://mindustry-tool.app";
     public static final String RULE_URL = MINDUSTRY_TOOL_URL + "/rules";
